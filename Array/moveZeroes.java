5ec59aab51976cdbb16701b850d38e299e6b4f35
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2024-12-23 10:50:22 +0530

    moveZeroes_Array_Dt:23122024

---
+++ Array/moveZeroes.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class moveZeroes {
+    public static void main(String[] args) {
+
+        int nums[] = { 1, 0, 2, 0, 5, 0, 4, 89, 0 };
+        int leftPointer = 0;
+        int container = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                container = nums[leftPointer];
+                nums[leftPointer] = nums[i];
+                nums[i] = container;
+                leftPointer++;
+            }
+
+        }
+    }
+}
