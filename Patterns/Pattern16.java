5d525f892f12cadc5e8876d3be44b1953e73dfe2
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2024-12-27 11:09:27 +0530

    patterns_LogicalThinking_Dt:28122024

---
+++ Patterns/Pattern16.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Pattern16 {
+    public static void main(String args[]) {
+
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print((char) (65 + j) + " ");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
