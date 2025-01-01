68b97828b5b75d15d1b97989343a8f2ae24950a5
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-01-01 23:12:40 +0530

    patterns_LogicalThinking_Dt:01012025

---
+++ Patterns/Pattern19.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+public class Pattern19 {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (64 + n + j - i) + " ");
+            }
+
+            for (int j = 4; j > i; j--) {
+                System.out.print("");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
