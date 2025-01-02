6cb687c2f58182a9975d08a7eb6226359d27da25
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-01-02 22:49:20 +0530

    patterns_LogicalThinking_Dt:02012025

--- code.java
+++ code.java
@@ -4,38 +4,44 @@ class code {
 
         int n = 3;
 
-        for (int i = 0; i < 2 * n; i++) {
+        for (int i = 0; i < 2 * n - 1; i++) {
 
-            if (i < n) {
+            if (i < n - 1) {
 
-                for (int j = 0; j < n - i; j++) {
-                    System.out.print("a");
-                }
-                for (int j = 0; j < i; j++) {
-                    System.out.print("b");
-                }
-                for (int j = 0; j < i; j++) {
-                    System.out.print("c");
-                }
-                for (int j = 0; j < n - i; j++) {
-                    System.out.print("d");
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
                 }
 
+                for (int j = 0; j < n - i - 1; j++) {
+                    System.out.print("  ");
+                }
+
+                for (int j = 0; j < n - i - 1; j++) {
+                    System.out.print("  ");
+                }
+
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+
+            } else if (i == n - 1) {
+                for (int j = 0; j < 2 * n; j++) {
+                    System.out.print("* ");
+                }
             } else {
 
-                for (int j = 0; j <= i - n; j++) {
-                    System.out.print("a");
-                }
-                for (int j = 0; j <= n - i + 1; j++) {
-                    System.out.print("b");
-                }
-                for (int j = 0; j <= n - i + 1; j++) {
-                    System.out.print("c");
+                for (int j = 0; j < (2 * n) - i - 1; j++) {
+                    System.out.print("* ");
                 }
                 for (int j = 0; j <= i - n; j++) {
-                    System.out.print("d");
+                    System.out.print("  ");
                 }
-
+                for (int j = 0; j <= i - n; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 0; j < (2 * n) - i - 1; j++) {
+                    System.out.print("* ");
+                }
             }
 
             System.out.println();
