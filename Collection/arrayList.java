b4865f1636e54b88c16a035b0929c19cb25d2ac1
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-08-14 09:35:50 +0530

    ArrayList_Dt:14082025

---
+++ Collection/arrayList.java
@@ -0,0 +1,30 @@
+package Collection;
+
+import java.util.*;
+
+public class arrayList {
+
+    public static void main(String args[]) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(2, 40);
+        list.add(30);
+
+        list.set(3, 300);
+
+        // list.remove(2); // will remove the index
+
+        // list.remove(Integer.valueOf(300));
+
+        // list.clear();
+
+        System.out.println(list.get(2));
+
+        System.out.println(list);
+
+    }
+
+}
