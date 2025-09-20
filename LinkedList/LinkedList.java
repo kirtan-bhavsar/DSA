10198adddff9574a80dcc189abe24a59d657b606
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-09-21 00:02:41 +0530

    linkedList_Dt:21092025

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,173 @@
+package LinkedList;
+
+public class LinkedList {
+
+    Node head;
+    Node tail;
+
+    public void deletePosition(int length, int position) {
+
+        if (position > length)
+            return;
+        if (position == 1) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        int currentPosition = 1;
+        Node prevTemp = null;
+
+        while (currentPosition != position) {
+
+            currentPosition += 1;
+            prevTemp = temp;
+            temp = temp.next;
+
+        }
+
+        prevTemp.next = temp.next;
+        temp.next = null;
+
+    }
+
+    public void deleteAllOccurances(int data) {
+
+        Node temp = head;
+        Node prevTemp = null;
+
+        while (temp.next != null) {
+
+            if (temp.data == data) {
+                if (prevTemp == null) {
+                    head = temp.next;
+                    temp.next = null;
+                    temp = head;
+                    continue;
+                }
+                temp = temp.next;
+                prevTemp.next = temp;
+                continue;
+            }
+
+            prevTemp = temp;
+            temp = temp.next;
+
+        }
+
+        if (temp.data == data) {
+            if (prevTemp == null) {
+                temp = null;
+                head = null;
+                return;
+            }
+            prevTemp.next = null;
+        }
+
+        return;
+
+    }
+
+    public void deleteSingleOccurance(int data) {
+
+        Node temp = head;
+        Node prevTemp = null;
+
+        while (temp.next != null) {
+
+            if (temp.data == data) {
+                if (prevTemp == null) {
+                    head = temp.next;
+                    temp.next = null;
+                    temp = head;
+                    return;
+                }
+                temp = temp.next;
+                prevTemp.next = temp;
+                return;
+            }
+
+            prevTemp = temp;
+            temp = temp.next;
+
+        }
+
+        if (temp.data == data) {
+            if (prevTemp == null) {
+                temp = null;
+                head = null;
+                return;
+            }
+            prevTemp.next = null;
+        }
+
+        return;
+
+    }
+
+    public int lengthOfLinkedList() {
+
+        Node temp = head;
+        int counter = 1;
+
+        while (temp.next != null) {
+            temp = temp.next;
+            counter += 1;
+        }
+
+        return counter;
+
+    }
+
+    public void add(int data) {
+
+        Node node = new Node();
+
+        node.data = data;
+        node.next = null;
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+
+    }
+
+    public void show() {
+
+        Node current = head;
+        if (current == null) {
+            System.out.println("There are no elements in linkedlist to show");
+            return;
+        }
+
+        while (current.next != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+
+        System.out.println(current.data + " ");
+
+    }
+
+    public int searchData(int data) {
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            if (temp.data == data)
+                return 1;
+            temp = temp.next;
+        }
+
+        if (temp.data == data)
+            return 1;
+
+        return 0;
+    }
+
+}
---
+++ LinkedList/Runner.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class Runner {
+
+    public static void main(String args[]) {
+
+        LinkedList list = new LinkedList();
+
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(1);
+        list.add(1);
+        list.add(7);
+        list.add(7);
+        list.add(7);
+        list.add(7);
+
+        // list.show();
+        System.out.println(list.lengthOfLinkedList() + " is the length of the linked list");
+        System.out.println(list.searchData(2));
+        System.out.println(list.searchData(33));
+
+        // list.deletePosition(list.lengthOfLinkedList(), 5);
+        // list.show();
+        list.show();
+        // list.deleteAllOccurances(2);
+        list.deleteSingleOccurance(1);
+        list.show();
+
+    }
+
+}
