8820783b35234c10352ae2495b6a23bb9430445a
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2026-02-06 08:52:01 +0530

    DoublyLinkedList -> Insertions

--- LearnLinkedList/DoublyLinkedList.java
+++ LearnLinkedList/DoublyLinkedList.java
@@ -4,21 +4,18 @@ public class DoublyLinkedList {
 
     DoublyNode head;
 
+    // also insert at Tail
     public void add(int data) {
-
-        System.out.println("add called for data : " + data);
 
         if (head == null) {
             head = new DoublyNode();
             head.data = data;
             head.prevNode = null;
             head.nextNode = null;
-            System.out.println("add ended when inserting the head");
             return;
         }
 
         DoublyNode temp = head;
-        System.out.println("currently the temp is " + temp.data);
 
         while (temp.nextNode != null) {
             temp = temp.nextNode;
@@ -29,9 +26,101 @@ public class DoublyLinkedList {
         newNode.nextNode = null;
         newNode.prevNode = temp;
         temp.nextNode = newNode;
-        System.out.println("add ended for data : " + data);
     }
 
+    // insert at head
+    public void insertAtHead(int data) {
+
+        if (head == null) {
+            head = new DoublyNode();
+            head.data = data;
+            head.nextNode = null;
+            head.prevNode = null;
+            return;
+        } else {
+            DoublyNode newNode = new DoublyNode();
+            newNode.data = data;
+            newNode.nextNode = head;
+            newNode.prevNode = null;
+            head.prevNode = newNode;
+            head = newNode;
+            return;
+        }
+
+    }
+
+    // insert at Kth Node(position)
+    public void insertAtPosition(int data, int position) {
+
+        if (head == null) {
+            if (position > 1)
+                return;
+            head = new DoublyNode();
+            head.data = data;
+            head.nextNode = null;
+            head.prevNode = null;
+            return;
+        }
+
+        if (position == 1) {
+            DoublyNode newNode = new DoublyNode();
+            newNode.data = data;
+            newNode.nextNode = head;
+            newNode.prevNode = null;
+            head.prevNode = newNode;
+            head = newNode;
+            return;
+        }
+
+        DoublyNode temp = head;
+        int counter = 1;
+        DoublyNode nextTemp;
+
+        while (temp.nextNode != null) {
+            nextTemp = temp.nextNode;
+            if (counter == position - 1) {
+                DoublyNode newNode = new DoublyNode();
+                newNode.data = data;
+                temp.nextNode = newNode;
+                nextTemp.prevNode = newNode;
+                newNode.prevNode = temp;
+                newNode.nextNode = nextTemp;
+                return;
+            }
+            temp = nextTemp;
+            counter += 1;
+        }
+
+        if (counter == position - 1) {
+            DoublyNode newNode = new DoublyNode();
+            newNode.data = data;
+            newNode.nextNode = null;
+            newNode.prevNode = temp;
+            temp.nextNode = newNode;
+            return;
+        }
+
+    }
+
+    // Convert array to doubly linkedlist
+    public DoublyLinkedList arrayToDoublyLinkedList(int arr[]) {
+
+        DoublyLinkedList dll = new DoublyLinkedList();
+
+        if (arr.length == 0)
+            return dll;
+
+        dll.insertAtHead(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            dll.add(arr[i]);
+        }
+
+        return dll;
+
+    }
+
+    // To display linkedlist in a visual format
     public void show() {
 
         if (head == null) {
--- LearnLinkedList/DoublyRunner.java
+++ LearnLinkedList/DoublyRunner.java
@@ -6,9 +6,83 @@ public class DoublyRunner {
 
         DoublyLinkedList doublylist = new DoublyLinkedList();
 
-        doublylist.add(1);
-        // doublylist.add(2);
-        // doublylist.add(3);
+        // doublylist.add(1);
+        // // doublylist.add(2);
+        // // doublylist.add(3);
+        // doublylist.show();
+
+        // ---Array to linked list test cases---
+
+        // Test Case 1
+        // int arr[] = { 1, 2, 3, 4 }; // also passed for empty array, with
+        // one/two/three element(s)
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+
+        // ---Insert at head test cases---
+
+        // Test Case 1
+        // int arr[] = {};
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // doublylist.insertAtHead(1);
+        // doublylist.show();
+
+        // Test Case 2
+        // int arr[] = { 2 };
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // doublylist.insertAtHead(1);
+        // doublylist.show();
+
+        // // Test Case 3
+        // int arr[] = { 2, 3 };
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // doublylist.insertAtHead(1);
+        // doublylist.show();
+
+        // // Test Case 4
+        // int arr[] = { 2, 3, 4 };
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // doublylist.insertAtHead(1);
+        // doublylist.show();
+
+        // ---insertAtPosition test cases---
+        // Test Case 1
+        // int arr[] = {};
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // // doublylist.insertAtPosition(99, 1);
+        // // doublylist.insertAtPosition(99, 2);
+        // doublylist.show();
+
+        // Test Case 2
+        // int arr[] = {};
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // doublylist.insertAtPosition(99, 2);
+        // doublylist.show();
+
+        // Test Case 3
+        // int arr[] = { 1 };
+        // doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        // doublylist.show();
+        // // doublylist.insertAtPosition(99, 2);
+        // // doublylist.insertAtPosition(99, 1);
+        // // doublylist.insertAtPosition(99, 3);
+        // doublylist.show();
+
+        // Test Case 4
+        int arr[] = { 1, 2, 3 };
+        doublylist = doublylist.arrayToDoublyLinkedList(arr);
+        doublylist.show();
+        // doublylist.insertAtPosition(99, 1);
+        // doublylist.insertAtPosition(99, 2);
+        // doublylist.insertAtPosition(99, 3);
+        // doublylist.insertAtPosition(99, 4);
+        doublylist.insertAtPosition(99, 5);
         doublylist.show();
 
     }
