475d65f5e7b0b0dd15526fb76ab2a12742fdb924
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2026-02-21 19:22:12 +0530

    LinkedList -> Add1ToLinkedListOptimal

--- LearnLinkedList/Add1ToLinkedList.java
+++ LearnLinkedList/Add1ToLinkedList.java
@@ -17,67 +17,54 @@ public class Add1ToLinkedList {
 
             Node temp = head;
             Node prevTemp = null;
-            Node seriesStart = null;
             Node prevSeries = null;
-            boolean doesSeriesExists = false;
 
             while (temp != null) {
+
                 if (temp.data == 9) {
-                    if (temp != head
-                            && prevSeries == null
-                            && prevTemp.data != 9)
+                    if (temp != head && prevTemp != null && prevTemp.data != 9)
                         prevSeries = prevTemp;
-                    if (seriesStart == null)
-                        seriesStart = temp;
-                    doesSeriesExists = true;
-
                     prevTemp = temp;
                     temp = temp.next;
-                    continue;
                 } else {
-                    seriesStart = null;
                     prevSeries = null;
-                    doesSeriesExists = false;
                     prevTemp = temp;
                     temp = temp.next;
-                    continue;
                 }
+
             }
 
-            if (!doesSeriesExists) {
-                if (prevTemp == null) {
-                    head.data += 1;
-                    return head;
-                } else {
-                    prevTemp.data += 1;
-                    return head;
+            // whole 9 series
+            if (prevSeries == null && prevTemp.data == 9) {
+                temp = head;
+                temp.data = 1;
+                while (temp.next != null) {
+                    temp = temp.next;
+                    temp.data = 0;
                 }
-            }
-
-            if (prevSeries != null && doesSeriesExists) {
-                prevSeries.data += 1;
-                while (seriesStart != null) {
-                    seriesStart.data = 0;
-                    seriesStart = seriesStart.next;
-                }
+                Node newNode = new Node(0);
+                temp.next = newNode;
                 return head;
             }
 
-            if (prevSeries == null && doesSeriesExists) {
-                head.data = 1;
-                seriesStart = seriesStart.next;
-                while (seriesStart != null) {
-                    seriesStart.data = 0;
-                    seriesStart = seriesStart.next;
+            // although 9 series exists but not whole
+            if (prevSeries != null && prevTemp.data == 9) {
+
+                temp = prevSeries;
+                temp.data += 1;
+
+                while (temp.next != null) {
+                    temp = temp.next;
+                    temp.data = 0;
                 }
-                Node newNode = new Node(0);
-                if (prevTemp == null) {
-                    head.next = newNode;
-                    return head;
-                } else {
-                    prevTemp.next = newNode;
-                    return head;
-                }
+
+                return head;
+            }
+
+            // no 9 at last
+            if (prevTemp.data != 9) {
+                prevTemp.data += 1;
+                return head;
             }
 
             return head;
@@ -85,4 +72,74 @@ public class Add1ToLinkedList {
         }
     }
 
+    public Node addOne(Node head) {
+
+        Node temp = head;
+        Node prevTemp = null;
+        Node seriesStart = null;
+        Node prevSeries = null;
+        boolean doesSeriesExists = false;
+
+        while (temp != null) {
+            if (temp.data == 9) {
+                if (temp != head
+                        && prevSeries == null
+                        && prevTemp.data != 9)
+                    prevSeries = prevTemp;
+                if (seriesStart == null)
+                    seriesStart = temp;
+                doesSeriesExists = true;
+
+                prevTemp = temp;
+                temp = temp.next;
+                continue;
+            } else {
+                seriesStart = null;
+                prevSeries = null;
+                doesSeriesExists = false;
+                prevTemp = temp;
+                temp = temp.next;
+                continue;
+            }
+        }
+
+        if (!doesSeriesExists) {
+            if (prevTemp == null) {
+                head.data += 1;
+                return head;
+            } else {
+                prevTemp.data += 1;
+                return head;
+            }
+        }
+
+        if (prevSeries != null && doesSeriesExists) {
+            prevSeries.data += 1;
+            while (seriesStart != null) {
+                seriesStart.data = 0;
+                seriesStart = seriesStart.next;
+            }
+            return head;
+        }
+
+        if (prevSeries == null && doesSeriesExists) {
+            head.data = 1;
+            seriesStart = seriesStart.next;
+            while (seriesStart != null) {
+                seriesStart.data = 0;
+                seriesStart = seriesStart.next;
+            }
+            Node newNode = new Node(0);
+            if (prevTemp == null) {
+                head.next = newNode;
+                return head;
+            } else {
+                prevTemp.next = newNode;
+                return head;
+            }
+        }
+
+        return head;
+
+    }
 }
