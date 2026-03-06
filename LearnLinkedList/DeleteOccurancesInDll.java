7da6eda945577a0f2cbb2beccb496925735620dc
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2026-03-06 08:06:16 +0530

    DoblyLinkedList -> DeleteAllOccurances

---
+++ LearnLinkedList/DeleteOccurancesInDll.java
@@ -0,0 +1,60 @@
+package LearnLinkedList;
+
+public class DeleteOccurancesInDll {
+
+    public static void main(String args[]) {
+
+        int arr[] = { 3, 2 };
+
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list = list.arrayToDoublyLinkedList(arr);
+
+        list.show();
+
+        int k = 3;
+
+        DoublyNode temp = list.head;
+
+        while (temp.nextNode != null) {
+
+            if (temp.data == k) {
+
+                if (temp == list.head) {
+                    if (temp.nextNode != null) {
+                        temp = temp.nextNode;
+                        list.head = temp;
+                        temp.prevNode = null;
+                        continue;
+                    }
+                }
+
+                if (temp != list.head) {
+                    temp.prevNode.nextNode = temp.nextNode;
+                }
+
+                if (temp.nextNode != null) {
+                    temp.nextNode.prevNode = temp.prevNode;
+                }
+
+                temp = temp.nextNode;
+            } else {
+                temp = temp.nextNode;
+            }
+
+        }
+
+        if (temp.data == k) {
+            if (temp != list.head) {
+                temp.prevNode.nextNode = null;
+            } else {
+                list.head = null;
+            }
+        }
+
+        // return list.head;
+        list.show();
+
+    }
+
+}
