d180e58a326275ad4660cded6a7937b280e06978
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2026-03-14 17:15:31 +0530

    Strings -> AnagramOfStrings

---
+++ Strings/AnagramRev.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnagramRev {
+
+    class Solution {
+        public boolean isIsomorphic(String s, String t) {
+
+            Map<Character, Character> hashMap = new HashMap<>();
+
+            for (int i = 0; i < s.length(); i++) {
+
+                if (hashMap.containsKey(s.charAt(i)) || hashMap.containsValue(t.charAt(i))) {
+
+                    if (hashMap.containsValue(t.charAt(i)) && !hashMap.containsKey(s.charAt(i))
+                            ||
+                            (hashMap.containsKey(s.charAt(i)) && hashMap.get(s.charAt(i)) != t.charAt(i)))
+                        return false;
+                    else
+                        continue;
+
+                }
+
+                hashMap.put(s.charAt(i), t.charAt(i));
+
+            }
+
+            return true;
+
+        }
+    }
+
+}
